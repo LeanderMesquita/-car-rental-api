f3f17799dcbc2af1c5b237ee49b9d1d511e2b7c6
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-01-15 18:03:19 -0300

    adjust api routes

--- routes/api.php
+++ routes/api.php
@@ -18,21 +18,10 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+//Route::apiResource('modelo', 'App\Http\Controllers\ModeloController')->middleware('jwt.auth');
 
-Route::apiResource('cliente', 'App\Http\Controllers\ClienteController');
-Route::apiResource('carro', 'App\Http\Controllers\CarroController');
-Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
-Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
-Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
+Route::prefix('auth')->middleware('jwt.auth')->group(function ($router) {
 
-Route::group([
-
-    'middleware' => 'api',
-    'prefix' => 'auth'
-
-], function ($router) {
-
-    Route::post('login', 'App\Http\Controllers\AuthController@login');
     Route::post('logout', 'App\Http\Controllers\AuthController@logout');
     Route::post('refresh', 'App\Http\Controllers\AuthController@refresh');
     Route::post('me', 'App\Http\Controllers\AuthController@me');
@@ -42,8 +31,10 @@ Route::group([
     Route::apiResource('marca', 'App\Http\Controllers\MarcaController');
     Route::apiResource('modelo', 'App\Http\Controllers\ModeloController');
     Route::apiResource('locacao', 'App\Http\Controllers\LocacaoController');
-
-
+    
+    
 });
-// Route::post('login', 'App\Http\Controllers\AuthController@login');
+
+Route::post('login', 'App\Http\Controllers\AuthController@login');
+
 
