73bca93ad0d002f74fcd66d60e8529bc915120fc
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-01-25 16:49:38 -0300

    Ajustes no Auth e autenticação session

--- config/auth.php
+++ config/auth.php
@@ -14,7 +14,7 @@ return [
     */
 
     'defaults' => [
-        'guard' => 'api',
+        'guard' => 'web',
         'passwords' => 'users',
     ],
 
@@ -43,6 +43,7 @@ return [
         'api' => [
             'driver' => 'jwt',
             'provider' => 'users',
+            'hash' => false,
         ],
     ],
 
