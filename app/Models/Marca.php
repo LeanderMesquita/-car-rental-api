86dbda6bf08b4e5c1001317902a3c1ab79152273
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-02-23 16:01:47 -0300

    bugfix feedback msg

--- app/Models/Marca.php
+++ app/Models/Marca.php
@@ -20,15 +20,16 @@ class Marca extends Model
 
     public function rules(){
         return [
-            'nome' => 'required', Rule::unique('nome')->ignore($this->id),
+            'nome' => 'required|unique:marcas', Rule::unique('nome')->ignore($this->id),
             'imagem' => 'required|file|mimes:png',
         ];
     }
     public function feedback(){
         return [
-            'required' => 'O campo :attribute é obrigatório',
-            'imagem.mimes' => 'O arquivo deve ser uma imagem do tipo PNG',
-            'nome.unique' => 'O nome da marca já existe',
+            'required' => 'O campo ":attribute" é obrigatório.',
+            'imagem.mimes' => 'O arquivo deve ser uma imagem do tipo PNG.',
+            'imagem.file' => 'O campo ":attribute" é obrigatório.',
+            'nome.unique' => 'O nome da marca já existe.',
         ];
     }
 
