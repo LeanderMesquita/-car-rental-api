04fbd7ddad53159093f7bf2455d5b98fbf742227
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-02-23 11:45:13 -0300

    alert component creation

---
+++ resources/js/components/utilities/AlertComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div :class="alertStyle" role="alert">
+        {{title}}
+        <hr>
+        <span v-if="details.data.message"> {{ details.data.message }}</span>
+        <span v-if="details.data.id"> {{ "ID do registro: "+details.data.id }}</span>
+        <br>
+        <ul v-if="details.data.errors">
+            <li v-for="i, key in details.data.errors" :key="key">{{ i[0] }}</li>
+        </ul>
+        
+    </div>
+</template>
+
+<script>
+    export default{
+        props: ['type', 'title', 'details'],
+        computed: {
+            alertStyle(){
+                return 'alert alert-'+this.type;
+            }
+        }
+    }
+</script>
