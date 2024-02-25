aacf4c14f41aa00677b1e99dc1c95f15349c192f
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-02-25 17:40:22 -0300

    removing dependencies in table component

--- resources/js/components/utilities/TableComponent.vue
+++ resources/js/components/utilities/TableComponent.vue
@@ -3,14 +3,15 @@
         <table class="table table-hover">
             <thead>
                 <tr>
-                    <th scope="col" v-for="t, key in titles" :key="key">{{ t }}</th>
+                    <th scope="col" v-for="t, key in titles" :key="key">{{ t.title }}</th>
                 </tr>
             </thead>
             <tbody>
-                <tr v-for="e in info" :key="e.id">
-                    <th scope="row">{{ e.id }}</th>
-                    <td>{{ e.nome }}</td>
-                    <td><img :src="'/storage/'+e.imagem" alt="" width="45px" height="45px"></td>
+                <tr v-for="obj, k in infoFilter" :key="k">
+                    <td v-for="value, kv in obj" :key="kv">
+                        <span v-if="titles[kv].type == 'text'">{{ value }}</span>
+                        <span v-if="titles[kv].type == 'image'"><img :src="'/storage/'+value" width="30" height="30" alt="img"></span>
+                    </td> 
                 </tr>
             </tbody>
         </table>
@@ -19,7 +20,24 @@
 
 <script>
 export default {
-    props: ['info', 'titles']
+    props: ['info', 'titles'],
+    computed: {
+       infoFilter() {
+            let fields = Object.keys(this.titles)
+            let filtredInfo = []
+
+            this.info.map((element)=>{
+                let filtredElement = {}
+
+                fields.forEach(field => {
+                    filtredElement[field] = element[field]
+                })
+                filtredInfo.push(filtredElement)
+            })
+
+            return filtredInfo;
+       } 
+    }
     
 }
 </script>
