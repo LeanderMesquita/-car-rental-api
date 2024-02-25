53e2747d291e800b5982f0eab4c318a2f7cb3162
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-02-25 17:38:43 -0300

    paginate component creation

---
+++ resources/js/components/utilities/PaginateComponent.vue
@@ -0,0 +1,16 @@
+<template>
+    <nav aria-label="Page navigation example">
+        <ul class="pagination" style="cursor:pointer">
+            
+            <slot></slot>
+            
+        </ul>
+    </nav>    
+</template>
+<script>
+    export default {
+        mounted(){
+            
+        }
+    }
+</script>
