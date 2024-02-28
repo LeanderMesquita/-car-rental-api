e0c24662770b8fdde51cf2ff895a0431a6a2c300
Leander Mesquita <leander.lopes.mesquita@gmail.com>
2024-02-27 21:58:14 -0300

    table dependencies and interact btns

--- resources/js/components/utilities/TableComponent.vue
+++ resources/js/components/utilities/TableComponent.vue
@@ -4,6 +4,8 @@
             <thead>
                 <tr>
                     <th scope="col" v-for="t, key in titles" :key="key">{{ t.title }}</th>
+                    <th v-if="view || update || drop"></th>
+
                 </tr>
             </thead>
             <tbody>
@@ -12,6 +14,11 @@
                         <span v-if="titles[kv].type == 'text'">{{ value }}</span>
                         <span v-if="titles[kv].type == 'image'"><img :src="'/storage/'+value" width="30" height="30" alt="img"></span>
                     </td> 
+                    <td>
+                        <button v-if="view.visible" type="button" class="btn btn-outline-primary btn-sm mx-1" :data-bs-toggle="view.dataToggle" :data-bs-target="view.dataTarget"><i class="fa-solid fa-eye"></i></button>
+                        <button v-if="update" type="button" class="btn btn-outline-secondary btn-sm mx-1"><i class="fa-solid fa-pencil"></i></button>
+                        <button v-if="drop" type="button" class="btn btn-outline-danger btn-sm mx-1"><i class="fa-solid fa-trash"></i></button>
+                    </td>
                 </tr>
             </tbody>
         </table>
@@ -20,7 +27,7 @@
 
 <script>
 export default {
-    props: ['info', 'titles'],
+    props: ['info', 'titles', 'view', 'update', 'drop'],
     computed: {
        infoFilter() {
             let fields = Object.keys(this.titles)
